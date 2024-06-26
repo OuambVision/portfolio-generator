85a99341cd2a23c38ffc32474edf23494cacbd53
SedricOuambo <ouambosedric@gmail.com>
2024-06-26 15:09:12 -0400

    correct

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Test from "@/components/test";
+import Test from "@/components/Test";
 import Template1 from "@/components/Template1/Template1";
 
 export default function Home() {
