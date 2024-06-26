a50485d05e61519e3c7eb130eb6f306f89031e80
SedricOuambo <ouambosedric@gmail.com>
2024-06-26 19:52:06 -0400

    del

--- components/Template1/HomeSection/HomeSection.jsx
+++
@@ -1,8 +0,0 @@
-import styles from './HomeSection.module.css';
-import content from './homeSection.json';
-
-export default function HomeSection() {
-    return <>
-        <div>{content.nom}</div>
-    </>
-}
--- components/Template1/Template1.jsx
+++
@@ -1,6 +0,0 @@
-import HomeSection from "./HomeSection/HomeSection"
-export default function Template1() {
-    return <>
-        <HomeSection />
-    </>
-}
