d5f64e59c54ebd6b0a209c6c78da4ef5476c1222
SedricOuambo <ouambosedric@gmail.com>
2024-06-26 23:35:24 -0400

    again

--- next.config.js
+++ next.config.js
@@ -1,9 +1,35 @@
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    images:{
-        unoptimized: true
-    },
-    output: 'export'
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true
+  },
+  output: 'export'
 };
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio-generator' : '',
+  // Autres configurations Next.js
+};
+
+
+
+
+
+
+
+
+
+
+
+//   /** @type {import('next').NextConfig} */
+//   const nextConfig = {
+//     images:{
+//         unoptimized: true
+//     },
+//     output: 'export'
+// };
+
+// module.exports = nextConfig
