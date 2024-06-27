1235bea7f90178b2b27e6e506fff5168ba58bb9f
OuambVision <ouambvision@gmail.com>
2024-06-27 14:51:12 -0400

    zx

--- app/api/new-branch/route.js
+++ app/api/new-branch/route.js
@@ -41,7 +41,7 @@ export async function PUT(request) {
         const shaCommit = commits[0].sha;
         console.log(commits.length);
         for (let i = 0; i < commits.length; i++) {
-            console.log(i, commits[i].commit.message);
+            console.log((i+1), commits[i].commit.message);
         }
 
         // Créer une nouvelle branche
